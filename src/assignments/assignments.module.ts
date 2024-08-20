915b073a28c31831e338c5cd317e37f6b08db660
Beykun <bayurasukmaraga@gmail.com>
2024-08-20 13:45:07 +0700

    add create and find all assignment

--- src/assignments/assignments.module.ts
+++ src/assignments/assignments.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AssignmentsService } from './assignments.service';
 import { AssignmentsController } from './assignments.controller';
+import { DatabaseService } from '../utils/database/database.service';
+import { HelperService } from '../utils/helper/helper.service';
+import { DatabaseModule } from '../utils/database/database.module';
 
 @Module({
-  providers: [AssignmentsService],
-  controllers: [AssignmentsController]
+  providers: [AssignmentsService, DatabaseService, HelperService],
+  controllers: [AssignmentsController],
+  imports: [DatabaseModule],
 })
 export class AssignmentsModule {}
--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -3,15 +3,11 @@ import { DomainService } from './domain.service';
 import { DomainController } from './domain.controller';
 import { DatabaseModule } from '../utils/database/database.module';
 import { DatabaseService } from '../utils/database/database.service';
-import { HelperService } from 'src/utils/helper/helper.service';
+import { HelperService } from '../utils/helper/helper.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [DomainController],
-  providers: [
-    DomainService,
-    DatabaseService,
-    HelperService
-  ],
+  providers: [DomainService, DatabaseService, HelperService],
 })
 export class DomainModule {}
